5c0bb50c0ebc7610155a1d95fef162cec9b2b96d
BEKIRKSU <113244972+BEKIRKSU@users.noreply.github.com>
2023-02-17 18:46:22 +0000

    JS updated

--- src/componets/Form.jsx
+++ src/componets/Form.jsx
@@ -1,30 +1,49 @@
 import React from "react";
-import Form from "./Form";
 
-var userIsRegistered = false;
+//So we created and exported the props from the App.jsx file
+//to this form.jsx file. Now we check for it in order to render
+//something different inside our button. In this case what we want to do
+//is check to see if props.isRegistered (spell it right) is true, and if so
+//we want to show the word 'Login', but otherwise we're going to show the word
+//'register'. And make sure YOU WRAP IT INSIDE CURLY BRACES SO IT GETS
+//HIGHLIGHTED PROPERLY. Look below to see what it will look like.
 
-//To get this \/ar from App.jsx to form.jsx we can add it as a prop.
-//So let's create a prop below called, isRegistered and let's set it
-//equal to the \/alue of this \/ariable. Chekck below. This makes a lot
-//of sense.
-//Frequently when you're rendering things conditionally, you're going
-//to be doing it \/ia props that ha\/e been passed o\/er. (Need to look
-//further into props.)
-//So now inside our form component we can receie these props, which mean
-//simply writing function Form(props) - when you pass a prop into the
-//component in an imported file you basically export the prop and import
-//it into the file you exported the original component from. This sounds
-//confusing but it's not.
-//Whe the props are receied (props) we check for it in order to render
-//something different inside our button.
-//Check form.jsx for notes.
+//Once this is done, we can go back to app.js and switch this
+//\/ariable from false to true you'll see that, that goes to Login, and
+//when false we will see Register printed. (\/ar userIsRegisted = false/true)
 
-function App() {
+//Now to get rid of or keep the confirm password box we can use the AND operator.
+//Since this is an 'if' moment.
+//So we could open a set of parantheses and check if prop.isRegistered is
+//true or false. So if it's true nothing will be rendered.
+//Look below to see what it will look like.
+//So we check the property 'isRegistered', and if it's false we print
+//the confirm password. So basically
+//We can always test these out by changing true and false etc on App.js
+//This below could ha\/e also been written using a ternary operator:
+//{props.isRegistered === false ? <input type="password" placeholder =
+//"Confirm Password" /> : null}
+//But ofc if you want the most efficient way of creating this kind of
+//conditional rendering its going to be through using the ampersand
+//operator, by remoing the null and etc. Also, we can further shorten this by
+//writing {props.isRegistered === false && etc } like this
+// {!props.isRegistered && etc}
+//So we can end up with a one line statement that does e\/erything we want:
+//{!props.isRegistered && <input type="password" placeholder="Confirm Password"} />}
+
+//Conditional rendering is a fundamental part of React.
+
+function Form(props) {
   return (
-    <div className="container">
-      <Form isRegistered={userIsRegistered} />
-    </div>
+    <form className="form">
+      <input type="text" placeholder="Username" />
+      <input type="password" placeholder="Password" />
+      {props.isRegistered === false && (
+        <input type="password" placeholder="Confirm Password" />
+      )}
+      <button type="submit">{props.isRegistered ? "Login" : "Register"}</button>
+    </form>
   );
 }
 
-export default App;
+export default Form;
